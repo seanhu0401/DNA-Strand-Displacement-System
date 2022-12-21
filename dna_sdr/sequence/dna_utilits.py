07c80a6a670023d9b4f231eea01a298fd3acf3b7
seanhu0401 <seanhu0401@gmail.com>
2022-12-21 11:04:11 -0500

    added sequence comparison function

--- dna_sdr/sequence/dna_utilits.py
+++ dna_sdr/sequence/dna_utilits.py
@@ -1,6 +1,12 @@
 import regex as re
+from dna_class import DNA
 
 base_pairing = {"a": "t", "t": "a", "g": "c", "c": "g"}
+comp_dict = {"t": 1, "g": 2, "c": 3, "a": 4}
+
+
+def remove(seq: str):
+    return seq.replace(" ", "")
 
 
 def is_dna(seq: str):
@@ -12,5 +18,26 @@ def reverse_seq(seq: str):
     return seq[::-1]
 
 
-def remove(seq: str):
-    return seq.replace(" ", "")
+# *Think of better name for the method
+# This function is currently used to compare to sequence, mainly the triggers, to identify the location(s) that has/have mismatch(es)
+# They should be equal length in bases if there is no modification to the toehold + dangles - not applicable with those modification currently
+def sequence_comparison(seq_1: DNA, seq_2: DNA):
+    comparison_lst = []
+    base_loc_lst = []
+    counter = 0
+
+    if seq_1.base_count != seq_2.base_count:
+        raise Exception("The two sequences are not equal length")
+
+    while counter <= seq_1.base_count:
+        comp_val = 0
+        if seq_1.seq[counter] == seq_2.seq[counter]:
+            comparison_lst.append(comp_val)
+        elif seq_1.seq[counter] != seq_2.seq[counter]:
+            comp_val = comp_dict[seq_2[counter]]
+            comparison_lst.append(comp_val)
+        base_loc_lst.append(counter + 1)
+        counter += 1
+
+    seq_comp_dict = dict(zip(base_loc_lst, comparison_lst))
+    return seq_comp_dict
