f66c1331025f2f853504bbc71630bffa12bbafd2
seanhu0401 <seanhu0401@gmail.com>
2022-12-21 15:15:22 -0500

    redesign sequence utilites test

--- tests/test_sequence.py
+++ tests/test_sequence.py
@@ -3,12 +3,13 @@ from dna_sdr.sequence.dna_utilits import DNA
 from dna_sdr.sequence.seq_utilits import sequence_comparison
 
 
-t1 = DNA("TCT CA CAATC CA TCT")
-base_loc_lst = [loc + 1 for loc in range(t1.base_count)]
-zero_lst = [0] * t1.base_count
-zero_dict = dict(zip(base_loc_lst, zero_lst))
+@pytest.fixture
+def t1():
+    return DNA("TCT CA CAATC CA TCT")
 
 
-@pytest.mark.parametrize("seq1, seq2, expected", [(t1, t1, zero_dict)])
-def test_sequence_comparioson(seq1, seq2, expected):
-    assert sequence_comparison(seq1, seq2) == expected
+def test_same_sequence(t1):
+    zero_lst = [0] * t1.base_count
+    base_loc_lst = [loc + 1 for loc in range(t1.base_count)]
+    expected = dict(zip(base_loc_lst, zero_lst))
+    assert sequence_comparison(t1, t1) == expected
