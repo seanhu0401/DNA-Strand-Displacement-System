e791fa5ed7924d13763b395c0e3475da32e23fa1
seanhu0401 <seanhu0401@gmail.com>
2023-02-02 14:36:07 -0500

    basic seq_info gen and trig_df pickle output

--- dna_sdr/sequence/seq_utilits.py
+++ dna_sdr/sequence/seq_utilits.py
@@ -1,7 +1,20 @@
-from dna_sdr.sequence.dna_utilits import DNA
+from dna_sdr.sequence.dna_utilits import DNA, trigger
+import pandas as pd
+import os
+from more_itertools import consecutive_groups
 
 comp_dict = {"t": 1, "g": 2, "c": 3, "a": 4}
 base_pairing = {"a": "t", "t": "a", "g": "c", "c": "g"}
+
+
+def find_range(iterable):
+    for group in consecutive_groups(iterable):
+        group = list(group)
+        if len(group) == 1:
+            yield group[0]
+        else:
+            yield group[0], group[-1]
+
 
 # *Think of better name for the method
 # This function is currently used to compare to sequence, mainly the triggers, to identify the location(s) that has/have mismatch(es)
@@ -35,106 +48,144 @@ def sequence_comparison(seq_1: str, seq_2: str):
     return seq_comp_dict
 
 
-def strand_alignment_no_tb_mismatch(base_strand: DNA, incumbent: DNA, trigger: DNA):
-    bstrand = base_strand.seq
-    b_base = base_strand.base_count
-
-    b_incumb = incumbent.base_count
-
-    reverse_trig = trigger.reverse_seq()
-    b_trig = trigger.base_count
-
-    counter = mismatch_count = 0
-    last_match_loc = 0
-    tb_ob = b_trig - b_incumb  # number of bases for toehold + overhang
-    mismatch_loc = list()
-
-    for base in reverse_trig:
-        comp_base = base_pairing[base]
-        base_at_loc = bstrand[counter]
-
-        if comp_base == base_at_loc and counter <= b_base:
-            pass
-
-        elif comp_base != base_at_loc and b_trig - counter > tb_ob:
-            base_loc_5 = b_trig - counter
-            mismatch_loc.append(base_loc_5)
-            mismatch_count += 1
-
-        elif (
-            comp_base != base_at_loc
-            and b_trig - counter <= tb_ob
-            and counter > last_match_loc
-        ):
-            if last_match_loc < b_trig - counter:
-                last_match_loc = counter
-
-        counter += 1
-
-    if last_match_loc != 0:
-        toehold = last_match_loc - b_incumb
-        overhang = tb_ob - toehold
-
-    else:
-        toehold = tb_ob
-        overhang = 0
-
-    mismatch_loc.sort()
-
-    return mismatch_count, mismatch_loc, overhang, toehold
-
-
-def strand_alignment_tb_mismatch(base_strand: DNA, incumbent: DNA, trigger: DNA, tb: 7):
-    bstrand = base_strand.seq
-    b_base = base_strand.base_count
-
-    b_incumb = incumbent.base_count
-
-    reverse_trig = trigger.reverse_seq()
-    b_trig = trigger.base_count
-
-    counter = mismatch = 0
-    base_count = b_incumb + tb
-    mismatch_loc = list()
-
-    for base in reverse_trig:
-        comp_base = base_pairing[base]
-        base_at_loc = bstrand[counter]
-
-        if comp_base == base_at_loc and counter + 1 <= b_base:
-            pass
-
-        elif comp_base != base_at_loc and counter + 1 <= base_count:
-            base_loc_5 = b_trig - counter
-            mismatch_loc.append(base_loc_5)
-            mismatch += 1
-
-        counter += 1
-
-    overhang = b_trig - base_count
-    mismatch_loc.sort()
-
-    return mismatch, mismatch_loc, overhang
-
-
 def strand_alignment(
     base_strand: str, incumbent: str, trigger: str, tb_mismatch=False, tb=7
 ):
     try:
-        base = DNA(base_strand)
+        base_strand = DNA(base_strand)
         incumb = DNA(incumbent)
         trig = DNA(trigger)
     except ValueError:
         raise TypeError("Not all sequence given is DNA sequences")
 
+    bstrand = base_strand.seq
+    b_base = base_strand.base_count
+
+    b_incumb = incumb.base_count
+
+    reverse_trig = trig.reverse_complement()
+    b_trig = trig.base_count
+
+    counter = mismatch = 0
+    base_count = b_incumb + tb
+    mismatch_loc = list()
+
     if tb_mismatch:
-        mismatch_count, mismatch_loc, ob = strand_alignment_tb_mismatch(
-            base, incumb, trig, tb
-        )
+        for base in reverse_trig:
+            base_at_loc = bstrand[counter]
+
+            if base == base_at_loc and counter + 1 <= b_base:
+                pass
+
+            elif base != base_at_loc and counter + 1 <= base_count:
+                base_loc_5 = b_trig - counter
+                mismatch_loc.append(base_loc_5)
+                mismatch += 1
+
+            counter += 1
+
+        ob = b_trig - base_count
+        mismatch_loc.sort()
 
     else:
-        mismatch_count, mismatch_loc, ob, tb = strand_alignment_no_tb_mismatch(
-            base, incumb, trig
-        )
+        last_match_loc = 0
+        tb_ob = b_trig - b_incumb  # number of bases for toehold + overhang
 
-    return mismatch_count, mismatch_loc, ob, tb
+        for base in reverse_trig:
+            base_at_loc = bstrand[counter]
+
+            if base == base_at_loc and counter <= b_base:
+                pass
+
+            elif base != base_at_loc and b_trig - counter > tb_ob:
+                base_loc_5 = b_trig - counter
+                mismatch_loc.append(base_loc_5)
+                mismatch += 1
+
+            elif (
+                base != base_at_loc
+                and b_trig - counter <= tb_ob
+                and counter > last_match_loc
+                and last_match_loc < b_trig - counter
+            ):
+                last_match_loc = counter
+
+            counter += 1
+
+        if last_match_loc != 0:
+            toehold = last_match_loc - b_incumb
+            ob = tb_ob - toehold
+
+        else:
+            tb = tb_ob
+            ob = 0
+
+    mismatch_loc.sort()
+
+    return tb, ob, mismatch, mismatch_loc
+
+
+def name_generation(mismatch, toehold_b, overhang_b):
+    mismatch_groups = list(find_range(mismatch))
+
+    base_name = f"{toehold_b}tb_5'{overhang_b}ob"
+
+    for group in mismatch_groups:
+        try:
+            base_number = group[-1] - group[0] + 1
+            mismatch_name = f"_{group[0]}_{base_number}mb"
+        except TypeError:
+            base_number = 1
+            mismatch_name = f"_{group}_{base_number}mb"
+
+    try:
+        name = base_name + mismatch_name
+    except UnboundLocalError:
+        name = base_name
+
+    return name
+
+
+def seq_info(
+    fname,
+    incumb="CA CAATC CA TCT CA CCACC CA",
+    base_seq="TG GGTGG TG AGA TG GATTG TG AGA TG TG AGA CAT ACA GCG CCG ACC GTA",
+):
+    xls = pd.ExcelFile(fname)
+    master_list = list()
+
+    for sname in xls.sheet_names:
+        dna_seq = pd.read_excel(fname, sheet_name=sname)
+        print(sname)
+
+        if sname == "no_mtb":
+            toehold_bases = 7
+
+        elif sname == "mtb":
+            toehold_bases = int(input("toehold base amount:") or "7")
+
+        for seq in dna_seq["Seqences (5' to 3')"]:
+            tb, ob, mismatch, mismatch_loc = strand_alignment(
+                base_seq, incumb, seq, tb=toehold_bases
+            )
+            name = name_generation(mismatch_loc, tb, ob)
+            dna_trig = trigger(seq, name, tb, ob, mismatch, mismatch_loc)
+            master_list.append(dna_trig)
+
+    trig_df = pd.DataFrame([vars(trig) for trig in master_list])
+
+    return trig_df
+
+
+if __name__ == "__main__":
+    fname = "./dna_sdr/DNA_Strands.xlsx"
+    pickle_name = "./dna_sdr/pickles/trig_info.pkl"
+    df = seq_info(fname)
+    if os.path.exists(pickle_name):
+        trig_df = pd.read_pickle(pickle_name)
+        if df.equals(trig_df):
+            print("Same dataframe - no new file")
+        else:
+            df.to_pickle(pickle_name)
+    else:
+        df.to_pickle(pickle_name)
