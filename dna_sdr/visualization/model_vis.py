f38e311a4264e60d8530a15189ce63dc015432db
Sean Hu <seanhu0401@gmail.com>
2023-07-13 13:52:58 -0400

    remove old vis and update color cycle

--- dna_sdr/visualization/model_vis.py
+++ dna_sdr/visualization/model_vis.py
@@ -10,6 +10,18 @@ plt.rc("legend", fontsize=14)
 plt.rc("axes", labelsize=14)
 plt.rc("axes", titlesize=14)
 
+CB_color_cycle = [
+    "#377eb8",
+    "#ff7f00",
+    "#4daf4a",
+    "#f781bf",
+    "#a65628",
+    "#984ea3",
+    "#999999",
+    "#e41a1c",
+    "#dede00",
+]
+
 
 if __name__ == "__main__":
     os.chdir("./dna_sdr/pickles/")
