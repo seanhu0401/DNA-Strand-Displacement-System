439a43127ef27d5f0049a149c6ad5f2050ce609d
Sean Hu <seanhu0401@gmail.com>
2023-09-21 16:53:03 -0400

    reformat after pylint

--- dna_sdr/stats/art.py
+++ dna_sdr/stats/art.py
@@ -1,16 +1,17 @@
-import numpy as np
-import pandas as pd
-import scipy.stats as stats
-import itertools as it
-import math
-import os
-
 """
 The data input should have the response column to be the last column.
 """
+import math
+import os
+import itertools as it
+import numpy as np
+import pandas as pd
+# import scipy.stats as stats
+
+
 os.chdir("./")
-fname = "./Ignore/code/HigginsABC.art.csv"  # 3-way sample data w/ result
-df_full = pd.read_csv(fname)
+FNAME = "dna_sdr/stats/HigginsABC.art.csv"  # 3-way sample data w/ result
+df_full = pd.read_csv(FNAME)
 df = df_full.loc[:, ["A", "B", "C", "Y"]].copy()
 # print(df)
 
@@ -43,12 +44,12 @@ for i in range(n_ways):
     term_lst.append(term_loc)
 
 level_full_lst = list()
-for i in range(len(level_combo)):
-    combo_set = level_combo[i]
+for index, combo_set in enumerate(level_combo):
     combo_lst = [combo_set]
     for j in reversed(range(1, len(combo_set))):
         combo_lst.extend(list(it.combinations(combo_set, r=j)))
     level_full_lst.append(combo_lst)
+
 
 factor_full_lst = [tuple(factors)]
 for i in reversed(range(1, len(factors))):
@@ -71,9 +72,8 @@ full_est_effect_mean_lst = list()
 # Mean calculation for each level of effect combination
 for factor_level in level_full_lst:
     est_effect_mean_lst = list()
-    for location in range(len(factor_level)):
-        factor_combo = factor_full_lst[location]
-        level = factor_level[location]
+    for index, level in enumerate(factor_level):
+        factor_combo = factor_full_lst[index]
         df_copy = df.loc[:, factor_combo].copy()
         factor_copy = df_copy[df_copy.loc[:, factor_combo] == level].dropna()
         factor_indexes = factor_copy.index
@@ -83,55 +83,55 @@ for factor_level in level_full_lst:
 test_set = full_est_effect_mean_lst[0]
 param_array = np.zeros(len(test_set))
 
-for i in range(len(term_lst)):
-    if i - 1 < 0:
+for index, _ in enumerate(term_lst):
+    if index - 1 < 0:
         param_array[0] = test_set[0]
     else:
-        param_array[term_lst[i - 1]] = sum(test_set[term_lst[i - 1] : term_lst[i]])
+        param_array[term_lst[index - 1]] = sum(test_set[term_lst[index - 1] : term_lst[index]])
 
-for i in range(len(test_set)):
-    matrix[0:i, i] = param_array[i]
-    matrix[i, i] = test_set[i]
+for index, result in enumerate(test_set):
+    matrix[0:index, index] = param_array[index]
+    matrix[index, index] = result
 
 print(matrix)
 print(term_lst)
 # print(matrix[term_lst[0] : term_lst[1]])
 
-for i in range(len(term_lst)):
-    print()
+for index, location in enumerate(term_lst):
+    print(location)
 
-"""
-If the leading number of the set is zero, N-way is decrease by 1
-h increase each interval as the location group is updated and resets to
-0 after each loop. If leading number of the set is zero, h does not increase.
-"""
+# """
+# If the leading number of the set is zero, N-way is decrease by 1
+# h increase each interval as the location group is updated and resets to
+# 0 after each loop. If leading number of the set is zero, h does not increase.
+# """
 
-"""
-number_of_ways = len(factors)
-counts = 0
-for r in range(number_of_ways):
-    num = math.factorial(number_of_ways)
-    den = math.factorial(r) * math.factorial(number_of_ways - r)
-    counts += num / den
+# """
+# number_of_ways = len(factors)
+# counts = 0
+# for r in range(number_of_ways):
+#     num = math.factorial(number_of_ways)
+#     den = math.factorial(r) * math.factorial(number_of_ways - r)
+#     counts += num / den
 
-coef_matrix = [np.zeros((int(counts), int(counts)))]
-matrice_lst = coef_matrix * len(combo)
-"""
+# coef_matrix = [np.zeros((int(counts), int(counts)))]
+# matrice_lst = coef_matrix * len(combo)
+# """
 
 # Step 2: compute estimated effects for main + interaction effects
 # df_lst = list()
 
-"""
-for cond_num in range(len(combo)):
-    matrix = matrice_lst[cond_num]
-    i_index = 0
-    cond = combo[cond_num]
-    print(cond)
-    cond_df = df[df[factors].apply(tuple, 1).isin([cond])].copy()
-    factor_mean = cond_df["resp"].mean()
-    for i in range(len(cond)):
-        i_index += 1
-        print(cond[i])
-        cond_i_df = df[df[factors[i]] == cond[i]]
-        cond_i_mean = cond_i_df["resp"].mean()
-"""
+# """
+# for cond_num in range(len(combo)):
+#     matrix = matrice_lst[cond_num]
+#     i_index = 0
+#     cond = combo[cond_num]
+#     print(cond)
+#     cond_df = df[df[factors].apply(tuple, 1).isin([cond])].copy()
+#     factor_mean = cond_df["resp"].mean()
+#     for i in range(len(cond)):
+#         i_index += 1
+#         print(cond[i])
+#         cond_i_df = df[df[factors[i]] == cond[i]]
+#         cond_i_mean = cond_i_df["resp"].mean()
+# """
