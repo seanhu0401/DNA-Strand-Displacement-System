1fe45c4aec23d60c689a2d9992cb66138a727b4a
Sean Hu <seanhu0401@gmail.com>
2023-09-22 16:41:42 -0400

    Initial imple. of matrix summation for ART

--- dna_sdr/stats/art.py
+++ dna_sdr/stats/art.py
@@ -6,6 +6,7 @@ import os
 import itertools as it
 import numpy as np
 import pandas as pd
+
 # import scipy.stats as stats
 
 
@@ -32,7 +33,7 @@ n_ways = len(factors)
 levels_lst = [list(set(df[factor])) for factor in factors]
 level_combo = list(it.product(*levels_lst))
 
-term_lst = list()
+term_lst = []
 for i in range(n_ways):
     term = int(
         math.factorial(n_ways) / (math.factorial(i) * math.factorial(n_ways - i))
@@ -43,7 +44,7 @@ for i in range(n_ways):
         term_loc = term_lst[-1] + term
     term_lst.append(term_loc)
 
-level_full_lst = list()
+level_full_lst = []
 for index, combo_set in enumerate(level_combo):
     combo_lst = [combo_set]
     for j in reversed(range(1, len(combo_set))):
@@ -55,7 +56,7 @@ factor_full_lst = [tuple(factors)]
 for i in reversed(range(1, len(factors))):
     factor_full_lst.extend(list(it.combinations(factors, r=i)))
 
-df_lst = list()
+df_lst = []
 for cond in level_combo:
     cond_df = df[df[factors].apply(tuple, 1).isin([cond])].copy()
     mean_response = cond_df.iloc[:, -1].mean()
@@ -67,11 +68,11 @@ df = pd.concat(df_lst).sort_index()
 matrix = np.zeros((term_lst[-1], term_lst[-1] + 1))
 matrix[:, -1] = mean
 
-full_est_effect_mean_lst = list()
+full_est_effect_mean_lst = []
 
 # Mean calculation for each level of effect combination
 for factor_level in level_full_lst:
-    est_effect_mean_lst = list()
+    est_effect_mean_lst = []
     for index, level in enumerate(factor_level):
         factor_combo = factor_full_lst[index]
         df_copy = df.loc[:, factor_combo].copy()
@@ -80,6 +81,8 @@ for factor_level in level_full_lst:
         est_effect_mean_lst.append(resp[factor_indexes].mean())
     full_est_effect_mean_lst.append(est_effect_mean_lst)
 
+# Step 2: compute estimated effects for main + interaction effects
+# TODO: generalize it - for loop over each level combo
 test_set = full_est_effect_mean_lst[0]
 param_array = np.zeros(len(test_set))
 
@@ -87,41 +90,53 @@ for index, _ in enumerate(term_lst):
     if index - 1 < 0:
         param_array[0] = test_set[0]
     else:
-        param_array[term_lst[index - 1]] = sum(test_set[term_lst[index - 1] : term_lst[index]])
+        param_array[term_lst[index - 1]] = sum(
+            test_set[term_lst[index - 1] : term_lst[index]]
+        )
 
 for index, result in enumerate(test_set):
     matrix[0:index, index] = param_array[index]
     matrix[index, index] = result
 
-print(matrix)
-print(term_lst)
-# print(matrix[term_lst[0] : term_lst[1]])
+# TODO: Try to reduce the nested loop
+value_lst = []
+for index, _ in enumerate(term_lst):
+    ways = n_ways - index
+    if index == 0:
+        sub_matrix = [matrix[index]]
+    else:
+        sub_matrix = matrix[term_lst[index - 1] : term_lst[index]]
 
-for index, location in enumerate(term_lst):
-    print(location)
+    for array in sub_matrix:
+        VALUE = 0
+        COUNTER = 1
+        for h in range(index, n_ways):
+            if VALUE == 0 and array[0] != 0:
+                VALUE = array[0]
+            elif VALUE == 0 and array[0] == 0:
+                VALUE = sum(array[: term_lst[h]])
 
-# """
-# If the leading number of the set is zero, N-way is decrease by 1
-# h increase each interval as the location group is updated and resets to
-# 0 after each loop. If leading number of the set is zero, h does not increase.
-# """
+            if COUNTER % 2 != 0 and h < (n_ways - 1):
+                VALUE -= sum(array[term_lst[h] : term_lst[h + 1]])
+            elif COUNTER % 2 == 0 and h < (n_ways - 1):
+                VALUE += sum(array[term_lst[h] : term_lst[h + 1]])
+            else:
+                pass
+            COUNTER += 1
 
-# """
-# number_of_ways = len(factors)
-# counts = 0
-# for r in range(number_of_ways):
-#     num = math.factorial(number_of_ways)
-#     den = math.factorial(r) * math.factorial(number_of_ways - r)
-#     counts += num / den
+        if ways % 2 == 0:
+            VALUE += array[-1]
+        else:
+            VALUE -= array[-1]
 
-# coef_matrix = [np.zeros((int(counts), int(counts)))]
-# matrice_lst = coef_matrix * len(combo)
-# """
+        value_lst.append(VALUE)
 
-# Step 2: compute estimated effects for main + interaction effects
+# value_test_lst = [-1.25, -130.25, -124.75, -118.25, -10.25, -4.5, -5.0]
+# print(value_lst)
+# print(value_test_lst == value_lst)
+
 # df_lst = list()
 
-# """
 # for cond_num in range(len(combo)):
 #     matrix = matrice_lst[cond_num]
 #     i_index = 0
@@ -134,4 +149,3 @@ for index, location in enumerate(term_lst):
 #         print(cond[i])
 #         cond_i_df = df[df[factors[i]] == cond[i]]
 #         cond_i_mean = cond_i_df["resp"].mean()
-# """
