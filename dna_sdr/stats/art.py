fa9c4be8d46c85bae5f1fd8f8bb64429014b769f
Sean Hu <seanhu0401@gmail.com>
2023-09-26 15:44:26 -0400

    modify aligned value calculation method

--- dna_sdr/stats/art.py
+++ dna_sdr/stats/art.py
@@ -4,6 +4,7 @@ The data input should have the response column to be the last column.
 import math
 import os
 import itertools as it
+from functools import reduce
 import numpy as np
 import pandas as pd
 
@@ -27,22 +28,21 @@ df = pd.DataFrame({"row": row, "col": col, "resp": resp})
 resp = df.iloc[:, -1]
 mean = resp.mean()
 
-# Step 1: compute residual
 factors = list(df.columns)[:-1]
 n_ways = len(factors)
 levels_lst = [list(set(df[factor])) for factor in factors]
 level_combo = list(it.product(*levels_lst))
 
-term_lst = [0]
-for i in range(n_ways):
-    term = int(
-        math.factorial(n_ways) / (math.factorial(i) * math.factorial(n_ways - i))
-    )
-    if i == 0:
-        term_loc = term
-    else:
-        term_loc = term_lst[-1] + term
-    term_lst.append(term_loc)
+# term_lst = [0]
+# for i in range(n_ways):
+#     term = int(
+#         math.factorial(n_ways) / (math.factorial(i) * math.factorial(n_ways - i))
+#     )
+#     if i == 0:
+#         term_loc = term
+#     else:
+#         term_loc = term_lst[-1] + term
+#     term_lst.append(term_loc)
 
 level_full_lst = []
 for index, combo_set in enumerate(level_combo):
@@ -55,83 +55,45 @@ factor_full_lst = [tuple(factors)]
 for i in reversed(range(1, len(factors))):
     factor_full_lst.extend(list(it.combinations(factors, r=i)))
 
+factor_combo_lst = []
+for factor in factor_full_lst:
+    factor_combo = []
+    for length in reversed(range(1, len(factor) + 1)):
+        factor_combo.extend(list(it.combinations(list(factor), r=length)))
+    factor_combo_lst.append(factor_combo)
+
+invert_levels = list(map(list, zip(*level_full_lst)))
+invert_levels = [list(set(levels)) for levels in invert_levels]
+factor_level_dict = dict(zip(factor_full_lst, invert_levels))
+
 df_lst = []
-for cond in level_combo:
-    cond_df = df[df[factors].apply(tuple, 1).isin([cond])].copy()
-    mean_response = cond_df.iloc[:, -1].mean()
-    cond_df["residual"] = cond_df.iloc[:, -1] - mean_response
-    df_lst.append(cond_df)
+for key, value in factor_level_dict.items():
+    sublist = []
+    for condition in value:
+        cond_df = df[df[list(key)].apply(tuple, 1).isin([condition])].copy()
+        cond_df[f"{key}"] = cond_df.iloc[:, -1].mean()
+        sublist.append(cond_df)
+    df_lst.append(pd.concat(sublist).sort_index())
 
-df = pd.concat(df_lst).sort_index()
+condition_mean_df = reduce(
+    lambda left, right: pd.merge(left, right, on=list(df.columns)), df_lst
+)
+cell_mean = condition_mean_df[str(factor_full_lst[0])]
+residual = resp - cell_mean
 
-matrix = np.zeros((term_lst[-1], term_lst[-1] + 1))
-matrix[:, -1] = mean
-
-full_est_effect_mean_lst = []
-
-# Mean calculation for each level of effect combination
-for factor_level in level_full_lst:
-    est_effect_mean_lst = []
-    for index, level in enumerate(factor_level):
-        factor_combo = factor_full_lst[index]
-        df_copy = df.loc[:, factor_combo].copy()
-        factor_copy = df_copy[df_copy.loc[:, factor_combo] == level].dropna()
-        factor_indexes = factor_copy.index
-        est_effect_mean_lst.append(resp[factor_indexes].mean())
-    full_est_effect_mean_lst.append(est_effect_mean_lst)
-
-# Step 2: compute estimated effects for main + interaction effects
-# TODO: generalize it - for loop over each level combo
-test_set = full_est_effect_mean_lst[0]
-param_array = np.zeros(len(test_set))
-
-for index in range(len(term_lst) - 1):
-    param_array[term_lst[index]] = sum(test_set[term_lst[index] : term_lst[index + 1]])
-
-for index, result in enumerate(test_set):
-    matrix[0:index, index] = param_array[index]
-    matrix[index, index] = result
-
-# TODO: Try to reduce the nested loop
-value_lst = []
-for index in range(len(term_lst) - 1):
-    ways = n_ways - index
-    sub_matrix = matrix[term_lst[index] : term_lst[index + 1]]
-    for array in sub_matrix:
-        COUNTER = 1
-        for h in range(index, n_ways):
-            if COUNTER == 1:
-                VALUE = sum(array[: term_lst[h + 1]])
-            elif COUNTER % 2 == 0 and h < n_ways:
-                VALUE -= sum(array[term_lst[h] : term_lst[h + 1]])
-            elif COUNTER % 2 != 0 and h < n_ways:
-                VALUE += sum(array[term_lst[h] : term_lst[h + 1]])
-            else:
-                pass
-            COUNTER += 1
-
-        if ways % 2 == 0:
-            VALUE += array[-1]
+aligned_df = df.copy()
+for factor_combo in factor_combo_lst:
+    n_way_comp = len(factor_combo[0])
+    for factor in factor_combo:
+        h = n_way_comp - len(factor)
+        if h == 0:
+            VALUE = condition_mean_df[str(factor)]
+        elif h % 2 == 0:
+            VALUE += condition_mean_df[str(factor)]
         else:
-            VALUE -= array[-1]
-
-        value_lst.append(VALUE)
-
-# print(value_lst)
-# value_test_lst = [-1.25, -130.25, -124.75, -118.25, -10.25, -4.5, -5.0]
-# print(value_test_lst == value_lst)
-
-# df_lst = list()
-
-# for cond_num in range(len(combo)):
-#     matrix = matrice_lst[cond_num]
-#     i_index = 0
-#     cond = combo[cond_num]
-#     print(cond)
-#     cond_df = df[df[factors].apply(tuple, 1).isin([cond])].copy()
-#     factor_mean = cond_df["resp"].mean()
-#     for i in range(len(cond)):
-#         i_index += 1
-#         print(cond[i])
-#         cond_i_df = df[df[factors[i]] == cond[i]]
-#         cond_i_mean = cond_i_df["resp"].mean()
+            VALUE -= condition_mean_df[str(factor)]
+    if n_way_comp % 2 == 0:
+        VALUE += mean
+    else:
+        VALUE -= mean
+    aligned_df[f"Aligned {factor_combo[0]}"] = VALUE + residual
