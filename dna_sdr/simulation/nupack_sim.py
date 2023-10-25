ea8c94c38feb196e980f7ac5e4ca4b7bf06a7205
Sean Hu <seanhu0401@gmail.com>
2023-10-25 13:41:08 -0400

    updated gitignore + docstring for nupack_sim

--- dna_sdr/simulation/nupack_sim.py
+++ dna_sdr/simulation/nupack_sim.py
@@ -1,8 +1,6 @@
-import pandas as pd
-from nupack import *
+"""
+DNA analysis using NUPACK
 
-
-""" 
 1X PBS Solution Concentrations:
     - NaCl: 137 mM
     - KCl: 2.7 mM
@@ -15,65 +13,74 @@ from nupack import *
     - MgCl2: 12 mM
 """
 
-"""
-The sum of the concentrations of (monovalent) sodium, potassium, and ammonium ions, 
-[Na+]+[K+]+[NH+4], is specified in units of molar using the keyword sodium.
-"""
+import pandas as pd
+import nupack
+
 
 if __name__ == "__main__":
-    folding_buffer_mg = 0.012  # [M] 1X folding buffer
-    pbs = (137 + 10 * 2 + 1.8 + 2.7) / 1000  # [M] 1X PBS
-    final_mg_concentration = folding_buffer_mg / 10  # 10x dilution in qPCR
+    # The sum of the concentrations of (monovalent) sodium, potassium, and ammonium ions,
+    # [Na+]+[K+]+[NH+4], is specified in units of molar using the keyword sodium.
+
+    MONOVALENT = (137 + 10 * 2 + 1.8 + 2.7) / 1000  # [M] 1X PBS
+    DIVALENT = 0.0012  # 0.1x folding buffer in qPCR
 
     # Salt is in [M]
-    model1 = Model(
+    model1 = nupack.Model(
         material="dna",
-        ensemble="stacking",
         celsius=40,
-        sodium=pbs,
-        magnesium=final_mg_concentration,
+        sodium=MONOVALENT,
+        magnesium=DIVALENT,
     )
 
-    fname = "./dna_sdr/pickles/trig_info.pkl"
-    trig_info_df = pd.read_pickle(fname)
-    trig_lst = trig_info_df["seq"].tolist()
-    name_lst = trig_info_df["name"].tolist()
+    FNAME = "./dna_sdr/pickles/trig_info.pkl"
+    trig_info_df: pd.DataFrame = pd.read_pickle(FNAME)
+    trig_lst: list[str] = trig_info_df["seq"].tolist()
+    name_lst: list[str] = trig_info_df["name"].tolist()
 
     trig_dict = dict(zip(name_lst, trig_lst))
 
-    base = Strand(
+    base = nupack.Strand(
         "TG GGTGG TG AGA TG GATTG TG AGA TG TG AGA CAT ACA GCG CCG ACC GTA".replace(
             " ", ""
         ),
         name="base",
     )
-    incumb = Strand(("CA CAATC CA TCT CA CCACC CA".replace(" ", "")), name="incumb")
+    incumb = nupack.Strand(
+        ("CA CAATC CA TCT CA CCACC CA".replace(" ", "")), name="incumb"
+    )
 
-    complex_list = list()
-    free_energy_list = list()
-    seq_list = list()
-    conc_list = list()
+    complex_list: list[str] = []
+    free_energy_list: list[float] = []
+    seq_list: list[str] = []
+    conc_list: list[float] = []
 
-    for i in range(len(trig_lst)):
-        trig = Strand((trig_lst[i]), name=name_lst[i])
-        trig_base_complex = "({}+base)".format(name_lst[i])
-        t1 = Tube(
-            strands={base: 5e-7, incumb: 5e-7, trig: 5e-7},
-            complexes=SetSpec(max_size=2),
-            name="t1",
-        )
-        complex_result = complex_analysis(complexes=t1, model=model1, compute=["pfunc"])
-        complex_list.append(trig_base_complex)
-        free_energy_list.append(complex_result[trig_base_complex].free_energy)
-        seq_list.append((trig_lst[i].replace(" ", "")))
-        complex_conc = complex_concentrations(tube=t1, data=complex_result)
-        for complex, conc in complex_conc["t1"].complex_concentrations.items():
-            if complex.name == trig_base_complex:
-                conc_list.append(conc * 10**9)
+    if trig_lst:
+        for index, trigger in enumerate(trig_lst):
+            trig = nupack.Strand((trigger), name=name_lst[index])
+            trig_base_complex = f"({name_lst[index]}+base)"
+            t1 = nupack.Tube(
+                strands={base: 5e-7, incumb: 5e-7, trig: 5e-7},
+                complexes=nupack.SetSpec(max_size=2),
+                name="t1",
+            )
+            complex_result = nupack.complex_analysis(
+                complexes=t1, model=model1, compute=["pfunc"]
+            )
+            complex_list.append(trig_base_complex)
+            free_energy_list.append(complex_result[trig_base_complex].free_energy)
+            seq_list.append((trigger.replace(" ", "")))
+            complex_conc = nupack.complex_concentrations(tube=t1, data=complex_result)
+            for complex_sturc, conc in complex_conc[
+                "t1"
+            ].complex_concentrations.items():
+                if complex_sturc.name == trig_base_complex:
+                    conc_list.append(conc * 10**9)
+    else:
+        raise ValueError()
 
     conc_dict = dict(zip(complex_list, conc_list))
     conc_df = pd.DataFrame.from_dict(conc_dict, orient="index", columns=["Conc (nM)"])
-    conc_df.to_pickle("./dna_sdr/pickles/concentration.pkl")
+    # conc_df.to_pickle("./dna_sdr/pickles/concentration.pkl")
 
     complex_list.append("(incumb+base)")
     seq_list.append(("CA CAATC CA TCT CA CCACC CA".replace(" ", "")))
@@ -83,5 +90,5 @@ if __name__ == "__main__":
     free_energy_df = pd.DataFrame.from_dict(
         free_energy_dict, orient="index", columns=["dG (kcal/mol)"]
     )
-    # free_energy_df["Seqences (5' to 3')"] = seq_list
-    free_energy_df.to_pickle("./dna_sdr/pickles/free_energy.pkl")
+    free_energy_df["Seqences (5' to 3')"] = seq_list
+    # free_energy_df.to_pickle("./dna_sdr/pickles/free_energy.pkl")
