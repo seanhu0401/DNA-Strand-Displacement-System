e5c53d80bf2f8fa88bb9daa8573976474abe9fe2
Sean Hu <seanhu0401@gmail.com>
2024-03-26 09:37:55 -0400

    remove irrelavent files

--- dna_sdr/simulation/sim_model.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def linear_function(x, slope, intercept):
-    return slope * x + intercept
-
-
-def logistic_function(x, L, k, xo):
-    return L / (1 + np.exp(-k * (x - xo)))
